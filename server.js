2896cce81f75adfe95d330f48a4d82b71822ab98
Javier Brea <javier.brea@gmail.com>
2019-04-30 21:01:37 +0200

    Sync url command

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const domapic = require('domapic-service')
 const options = require('./lib/options')
 const { Cloud } = require('./lib/Cloud')
 const { Users } = require('./lib/Users')
+const { Url } = require('./lib/Url')
 
 domapic.createPlugin({
   packagePath: path.resolve(__dirname),
@@ -14,8 +15,10 @@ domapic.createPlugin({
 }).then(plugin => {
   const cloud = Cloud(plugin)
   const users = Users(plugin, cloud)
+  const url = Url(plugin, cloud)
 
-  plugin.events.once('connection', users.sync)
+  plugin.events.on('connection', users.sync)
+  plugin.events.on('connection', url.sync)
   plugin.events.on('user:*', users.sync)
 
   return plugin.start()
