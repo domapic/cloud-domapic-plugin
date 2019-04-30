2bc84ef92e0c28f50f1a5eb9a71652980a3b3ec4
Javier Brea <javier.brea@gmail.com>
2019-04-30 20:03:25 +0200

    Retrieve Controller users. Retrieve server data from Cloud

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const path = require('path')
 
 const domapic = require('domapic-service')
 
+const options = require('./lib/options')
+const { Cloud } = require('./lib/Cloud')
+const { Users } = require('./lib/Users')
+
 domapic.createPlugin({
-  packagePath: path.resolve(__dirname)
-}).then(() => {
-  console.log('Domapic plugin created')
+  packagePath: path.resolve(__dirname),
+  customConfig: options
+}).then(plugin => {
+  const cloud = Cloud(plugin)
+  const users = Users(plugin, cloud)
+
+  plugin.events.once('connection', users.sync)
+  plugin.events.on('user:*', users.sync)
+
+  return plugin.start()
 })
